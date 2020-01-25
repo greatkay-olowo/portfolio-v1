98aff797c0390511e4d7f33519cdfca33d431aaf
Kayode Alao <oluwakayode.alao@gmail.com>
2020-01-25 19:36:27 +0100

    add template

---
+++ index.js
@@ -0,0 +1,17 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
+});
+
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', () => {
+  window.location = 'index.html#home';
+});
